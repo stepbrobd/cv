7f02d5fc6fffaf7cf5acf2014fc85951d180ba8f
Yifei Sun <ysun@hey.com>
2024-09-03 16:06:43 -0400

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/4c86138ce486d601d956a165e2f7a0fc029a03c1' (2024-04-26)
      → 'github:nixos/nixpkgs/b833ff01a0d694b910daca6e2ff4a3f26dee478c' (2024-09-01)

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     { self
     , nixpkgs
-    , flake-utils
-    }: flake-utils.lib.eachDefaultSystem (system:
+    , utils
+    }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
     in
