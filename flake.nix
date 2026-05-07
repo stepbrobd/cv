5175a4f71eca1485a1f33bdd675fcdc809e92b9c
Yifei Sun <ysun@hey.com>
2026-05-07 11:13:10 +0200

    move to triplet

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     parts.url = "github:hercules-ci/flake-parts";
     parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/triplet";
   };
 
   outputs = inputs: inputs.parts.lib.mkFlake { inherit inputs; } {
